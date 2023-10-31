dafba499ae30738b6e78e477fabfe88b6ab9b4c4
Sam Gao <samgao1999@163.com>
2023-10-31 13:38:40 -0700

    update on simple_control

--- ros/keyboard_control/keyboard_control/src/keyboard_control_node.cpp
+++ ros/keyboard_control/keyboard_control/src/keyboard_control_node.cpp
@@ -1,8 +1,90 @@
 #include <ros/ros.h>
 #include <mavros_msgs/OverrideRCIn.h>
-#include <mavros_msgs/RCIn.h>
 #include <sensor_msgs/Imu.h>
 #include <ncurses.h> // Include the ncurses library
+
+using namespace std
+
+class KeyboardControlNode
+{
+public:
+    KeyboardControlNode() : nh("~"), keyboard_reader(){
+        rc_override_pub = nh.advertise<mavros_msgs::OverrideRCIn>("/mavros/rc/override", 10);
+        imu_sub = nh.subscribe("/mavros/imu/data", 10, &KeyboardControlNode::imuCallback, this);
+        rc_override_sub = nh.subscribe("/mavros/rc/in", 10, &KeyboardControlNode::rcInCallback, this);
+        initialize_rc_mapping();
+        KeyboardInput keyboard_reader();
+    }
+
+    void imuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg){
+        // Callback function for processing IMU data
+        // You can add your IMU data processing code here
+        // For example, you can access imu_msg->orientation, imu_msg->angular_velocity, imu_msg->linear_acceleration, etc.
+    }
+
+    void rcInCallback(const mavros_msgs::RCIn::ConstPtr& rcIn_msg){
+        // Callback function for processing RC override In data
+        rcIn_channels = rcIn_msg->channels;
+        rcOverride_channels = rc_control_logic(rcIn_channels);
+        rc_override_msg.channels = rcOverride_channels; 
+    }
+
+    void rcOverridePublish(){
+        rc_override_pub.publish(rc_override_msg);
+    }
+
+    void initialize_rc_mapping(){
+        charToVectorMap['w']{0  , 0,  10, 0, 0, 0, 0, 0};
+        charToVectorMap['s']{0  , 0, -10, 0, 0, 0, 0, 0};
+        charToVectorMap['a']{-10, 0,  0, 0, 0, 0, 0, 0};
+        charToVectorMap['d']{10 , 0,  0, 0, 0, 0, 0, 0};
+    }
+
+    void rc_control_logic(vector<int> rcIn_channels){
+        ch = keyboard_reader.get_keyboard_input();
+        if (ch == 'q'){
+            // if "q" is pressed, switch to transmitter control
+            rcOverride_channels = std::vectors<int>{0, 0, 0, 0, 0, 0, 0, 0};
+        } 
+        else if (ch == ERR) {
+            // if no key is pressed, return to the default
+            rcOverride_channels = std::vectors<int>{1500, 0, 1400, 0, 0, 0, 0, 0};
+        } 
+        else{
+            if (charToVectorMap.find(ch) != charToVectorMap.end()) {
+                // if the key is within {"w","s","a","d"}
+                rcOverride_channels = rcIn_channels + charToVectorMap[ch];
+                // rcOverride_channels = rcIn_channels + charToVectorMap[ch];
+            }
+            else {
+                // Handle invalid keys
+                rcOverride_channels = std::vectors<int>{1500, 0, 1400, 0, 0, 0, 0, 0};
+            }
+        }
+        // the car need to handle multiple input as well
+        // tbd
+    }
+
+    std::vector<int> assign_channel_value(std::vector<int>& old_value, std::vector<int> new_value, int length){
+        for (int i = 0; i < length; ++i) {
+            old_value[i] = new_value[i];
+        }
+        return old_value;
+    }
+
+
+private:
+    ros::NodeHandle nh;
+    ros::Publisher rc_override_pub;
+    ros::Subscriber imu_sub;
+    mavros_msgs::OverrideRCIn rc_override_msg;
+    KeyboardInput keyboard_reader;
+    char ch;
+    std::map<char, std::vector<int>> charToVectorMap;
+    vector<int> rcIn_channels;
+    vector<int> rcOverride_channels;
+};
+
 
 class KeyboardInput
 {
@@ -28,87 +110,6 @@ public:
 
 private:
     char ch;
-};
-
-class KeyboardControlNode
-{
-public:
-    KeyboardControlNode() : nh("~"), keyboard_reader(){
-        rc_override_pub = nh.advertise<mavros_msgs::OverrideRCIn>("/mavros/rc/override", 10);
-        // imu_sub = nh.subscribe("/mavros/imu/data", 10, &KeyboardControlNode::imuCallback, this);
-        RCIn_sub = nh.subscribe("/mavros/rc/in", 10, &KeyboardControlNode::rcInCallback, this);
-        initialize_rc_mapping();
-    }
-
-    void imuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg){
-        // Callback function for processing IMU data
-        // You can add your IMU data processing code here
-        // For example, you can access imu_msg->orientation, imu_msg->angular_velocity, imu_msg->linear_acceleration, etc.
-    }
-
-    void rcInCallback(const mavros_msgs::RCIn::ConstPtr& rcIn_msg){
-        // Callback function for processing RC override In data
-	for (int i = 0; i < 8; ++i){
-		rcIn_channels[i] = rcIn_msg->channels[i];
-	}
-        rc_control_logic(rcIn_channels);
-	for (int i = 0; i < 8; ++i){
-		rc_override_msg.channels[i] = rcOverride_channels[i]; 
-	}
-    }
-
-    void rcOverridePublish(){
-        rc_override_pub.publish(rc_override_msg);
-    }
-
-    void initialize_rc_mapping(){
-        charToVectorMap['w'] = {0, 0, 10, 0, 0, 0, 0, 0};
-        charToVectorMap['s'] = {0, 0, -10, 0, 0, 0, 0, 0};
-        charToVectorMap['a'] = {-10, 0, 0, 0, 0, 0, 0, 0};
-        charToVectorMap['d'] = {10, 0, 0, 0, 0, 0, 0, 0};
-    }
-
-    void rc_control_logic(std::vector<int> rcIn_channels){
-        ch = keyboard_reader.get_keyboard_input();
-        if (ch == 'q'){
-            // if "q" is pressed, switch to transmitter control
-            rcOverride_channels = {0, 0, 0, 0, 0, 0, 0, 0};
-        } 
-        else if (ch == ERR) {
-            // if no key is pressed, return to the default
-            rcOverride_channels = {1500, 0, 1400, 0, 0, 0, 0, 0};
-        } 
-        else{
-            if (charToVectorMap.find(ch) != charToVectorMap.end()) {
-                // if the key is within {"w","s","a","d"}
-                rcOverride_channels = rcIn_channels;
-                for (int i = 0; i < 4; ++i)
-                {
-                    rcOverride_channels[i] += charToVectorMap[ch][i];
-                }
-            }
-            else {
-                // Handle invalid keys
-                rcOverride_channels = {1500, 0, 1400, 0, 0, 0, 0, 0};
-            }
-        }
-        // the car need to handle multiple input as well
-        // tbd
-    }
-
-
-
-private:
-    ros::NodeHandle nh;
-    ros::Publisher rc_override_pub;
-    // ros::Subscriber imu_sub;
-    mavros_msgs::OverrideRCIn rc_override_msg;
-    ros::Subscriber RCIn_sub;
-    KeyboardInput keyboard_reader;
-    char ch;
-    std::map<char, std::vector<int>> charToVectorMap;
-    std::vector<int> rcIn_channels;
-    std::vector<int> rcOverride_channels;
 };
 
 
